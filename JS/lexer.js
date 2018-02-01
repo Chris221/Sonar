b1367371440155728a44de07d39121e3be8c7f92
Chris Siena <zenatron@gmail.com>
2018-01-31 19:15:43 -0500

    Basic Lexer Start
    Intialize tokens
    
    Gathered the lines from the input
    
    Return tokens at the end

--- JS/lexer.js
+++ JS/lexer.js
@@ -3,4 +3,11 @@
 var log = $('#Lexer_log');
 
 function lexer(input) {
+	//defines tokens
+	var tokens = [];
+	
+	//takes the input and splits on carrage returns
+	var lines = input.trim().split("\n");
+	
+	return tokens;
 }
