ae69419a638bd6bde244943435d0400c62ef21f2
Chris Siena <zenatron@gmail.com>
2018-02-26 19:14:49 -0500

    Parser Logging should output
    Token info
    Pass/Fail
    Faol should show what was expected

--- JS/parser.js
+++ JS/parser.js
@@ -19,9 +19,31 @@ function parser(input) {
     parserLog("\n\nParser is starting..\n\n");
     //sets the token list
     tokens = input;
+//handles the parsering and CST
+function handle(token, unexpected = false) {
+    //sets the type of the token
+    var type = token.type;
+    //sets the value of the token
+    var value = token.value;
+    //sets the line of the token
+	var line = token.line;
+    //sets the col of the token
+    var column = token.col;
+
+    //Defines text
+    var text;
+
+    if (!unexpected) {
+        text = "Passed! Expected token found [ "+type+" ] with a value of [ "+value+" ] on line "+line+", "+column+"..."; 
+    } else {
+        text = "Failed! Unexpected token found [ "+type+" ]!\n Expected token [ "+unexpected+" ] on line "+line+", "+column+"...";  
+    }
+    parserLog(text);
+}
+
 function parserLog(text) {
     //Appends new logging to current log
-    var lText = $('#Lexer_log').val()+"LEXER -- "+text+"\n";
+    var lText = $('#Lexer_log').val()+"PARSER -- "+text+"\n";
     //Sets the Log
     $('#Lexer_log').text(lText);
 }
