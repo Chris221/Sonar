a612e54ded4a7a6e9abc3f63fb4a6ebcecf36e6b
Chris Siena <zenatron@gmail.com>
2018-02-26 19:29:31 -0500

    Compiler now calls the compileParser fucntion to handle the parser

--- JS/compiler.js
+++ JS/compiler.js
@@ -3,6 +3,7 @@
 //Debugger
 var debug = false;
 
+//Starts the compile
 function compile() {
 	//Clears the marquee for tokens
 	$('#token-marquee').text("");
@@ -37,10 +38,16 @@ function compile() {
 			//Adds token to the marquee
 			$('#token-marquee').append('<span class="token small"><span class="text-cyan">'+tCount+'</span> <span class="text-blue">:</span> <span class="text-red">'+tName+'</span> <span class="text-cyan">[</span> <span class="text-gray">'+tVal+'</span> <span class="text-cyan">]</span></span>');
 		}
+		compileParser(tokens);
 	} else {
 		//Sets marquee to failed text :(
 		$('#token-marquee').append('<span class="token small text-red">No tokens due to lexer error</span>');
 	}
 	//Outputs the Lexer output
 	$('#Lexer_log').text($('#Lexer_log').val()+"\n\n"+text+"\n\n");
+}
+
+//Moves the compiler to parse
+function compileParser(tokens) {
+	parser(tokens);
 }
