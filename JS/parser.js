6b3d2793000d46ee0db0c62f081579e56aa5cb93
Chris Siena <zenatron@gmail.com>
2018-02-26 19:04:51 -0500

    gets the current token

--- JS/parser.js
+++ JS/parser.js
@@ -1,8 +1,22 @@
 // JavaScript Document
 
+//defines the token list, current token and bracket level
 var tokens = [];
+var currentToken, bracketLevel;
+
+function getToken() {
+    //sets current token
+    currentToken = tokens[0];
+    //removes the token from the list
+    tokens.shift();
+    //returns the current token
+    return currentToken;
+}
+
 //runs the parser
 function parser(input) {
+    //Outputs starting
+    parserLog("\n\nParser is starting..\n\n");
     //sets the token list
     tokens = input;
 function parserLog(text) {
