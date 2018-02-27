8f876e056d75ac7b3e6f5bb3960c92635f6db175
Chris Siena <zenatron@gmail.com>
2018-02-26 19:04:25 -0500

    Parser's log
    currently the same log but may change

--- JS/parser.js
+++ JS/parser.js
@@ -1,7 +1,13 @@
 // JavaScript Document
 
 var tokens = [];
+//runs the parser
 function parser(input) {
     //sets the token list
     tokens = input;
+function parserLog(text) {
+    //Appends new logging to current log
+    var lText = $('#Lexer_log').val()+"LEXER -- "+text+"\n";
+    //Sets the Log
+    $('#Lexer_log').text(lText);
 }
