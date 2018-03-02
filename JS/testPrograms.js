f7279644def22443f412b2384ba438e262302364
Chris Siena <zenatron@gmail.com>
2018-03-02 05:26:14 -0500

    More Sample code
    cleaned up the sample code
    
    also added parse fails

--- JS/testPrograms.js
+++ JS/testPrograms.js
@@ -1,32 +1,36 @@
 // JavaScript Document
 
 //Project 1 clean
-var project1 = "/* Project 1 */\n"+
-	"{}$\n"+
-	"{{{{{{}}}}}}$\n"+
-	"{{{{{{}}}	/*	comments	are	ignored	*/	}}}}$\n"+
-	"{	/*	comments	are	still	ignored	*/	int	@}$";
+var project1 = "/* Sample Data */\n"+
+			   "{}$\n"+
+			   "{{{{{{}}}}}}$\n"+
+			   "{{{{{{}}}	/*	comments	are	ignored	*/	}}}}$\n"+
+			   "{	/*	comments	are	still	ignored	*/	int	@}$";
 
 //Project 1 ugly
-var project1Ugly = "/* Project 1 */"+
-		"{}$"+
-		"{{{{{{}}}}}}$"+
-		"{{{{{{}}}	/*	comments	are	ignored	*/	}}}}$"+
-		"{	/*	comments	are	still	ignored	*/	int	@}$";
+var project1Ugly = "/* Sample Data Ugly */"+
+				   "{}$"+
+				   "{{{{{{}}}}}}$"+
+				   "{{{{{{}}}	/*	comments	are	ignored	*/	}}}}$"+
+				   "{	/*	comments	are	still	ignored	*/	int	@}$";
 
 //Lex errors
 //Invalid characters
 var project1Lex = "/* Invalid Characters */\n"+
-				"{ int @}$";
+				  "{ int @}$";
 
 //Invalid break line in string
 var twoLineString = "/* Invalid Break Line in String */\n"+
-					"\"two\n"+
-					"lines\"$";
+					"{\n"+
+					"	\"two\n"+
+					"	lines\"\n"+
+					"}$";
 
 //Invalid string
 var invalidString = "/* Invalid String */\n"+
-					"\"this is good Not:`~!@#$%^&*()_+-=0987654321{}|[]\;'<>?,./\"$";
+					"{\n"+
+					"	\"this is good Not:`~!@#$%^&*()_+-=0987654321{}|[]\;'<>?,./\"\n"+
+					"}$";
 //Good simple cases
 var goodcase1 = "/* Simple good text case */\n"+
 				"{}$";
@@ -34,10 +38,62 @@ var goodcase1 = "/* Simple good text case */\n"+
 //Good case with string
 var goodcase2 = "/* Print a string */\n"+
 				"{\n"+
-				"string a\n"+
-				"a = \"this is a string\"\n"+
-				"print(a)\n"+
+				"	string a\n"+
+				"	a = \"this is a string\"\n"+
+				"	print(a)\n"+
 				"}$";
+
+/*
+ * These Parse Failure test cases came from Tien's test cases
+ */
+//invalid string
+var parseFail1 = "/* This will fail because an Identifier\n"+
+				 "- is expected but not provided */\n"+
+				 "{\n"+
+				 "	/* 1 is not a valid identifier */\n"+
+				 "	string 1\n"+
+				 "}$";
+				 
+//invalid expression
+var parseFail2 = "/* This will fail because you\n"+
+				 "- cannot add a int-type to a\n"+
+				 "- variable; however, you can\n"+
+				 "- add a variable to an int-type */\n"+
+				 "{\n"+
+				 "	a + 3\n"+
+				 "}$";
+
+//cannot print EOP
+var parseFail3 = "/* Printing an EPOS is an\n"+
+				 "- invalid operation that\n"+
+				 "- you should be killed for\n"+
+				 "- thinking that it would work */\n"+
+				 "{\n"+
+				 "	print($)\n"+
+				 "}$";
+
+//missing expression
+var parseFail4 = "/* There is a missing Expr on line 19\n"+
+				 "- that will cause this entire code to\n"+
+				 "- fail parse */\n"+
+				 "{\n"+
+				 "	int a\n"+
+				 "	int b\n"+
+				 "	a = 0\n"+
+				 "	b = 0\n"+
+				 "	while (a != 3) {\n"+
+				 "		print(a)\n"+
+				 "		while (b != 3) {\n"+
+				 "			print(b)\n"+
+				 "			b = 1 + b\n"+
+				 "			if (b == 2) {\n"+
+				 "				print(\"there is no spoon\")\n"+
+				 "			}\n"+
+				 "		}\n"+
+				 "		b = 0\n"+
+				 "		a = 1 + \n"+
+				 "	}\n"+
+				 "}$";
 
 //Output the testprogram
 function testProgram(name) {
@@ -68,6 +124,18 @@ function testProgram(name) {
 			break;
 		case "good_case_2":
 			rText = goodcase2;
+			break;
+		case "invalid_string_declare":
+			rText  = parseFail1;
+			break;
+		case "invalid_expression":
+			rText = parseFail2;
+			break;
+		case "print_EOP":
+			rText  = parseFail3;
+			break;
+		case "missing_expression":
+			rText = parseFail4;
 			break;
 		default:
 			rText = "";
