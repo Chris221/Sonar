c6144d0d1f580b51bea13d3cad926cee307b3f1b
Chris Siena <zenatron@gmail.com>
2018-05-04 02:22:38 -0400

    Log for code gen

--- JS/codegen.js
+++ JS/codegen.js
@@ -10,8 +10,28 @@ function gen(ast) {
     cWarnings = 0;
 
     code = generate();
-    
+
     return code;
+}
+
+//Sets the code log
+function codeLog(text, override = false) {
+    //Appends new logging to current log
+    var lText = $('#Lexer_log').val() + "#CODEGEN -- " + text + "\n";
+    //if verbose mode
+    if (!override) {
+        if (!verbose) {
+            //stops from ouputing
+            text = "DO NOT OUTPUT";
+        }
+    }
+    //if not supposed to be output
+    if (text == "DO NOT OUTPUT") {
+        //No need to change
+        lText = $('#Lexer_log').val();
+    }
+    //Sets the Log
+    $('#Lexer_log').text(lText);
 }
 
 function generate() {
