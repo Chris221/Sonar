fbf9ac85c92f0e3773abef0cf19e3336566c98b2
Chris Siena <zenatron@gmail.com>
2018-05-04 00:15:14 -0400

    Basic Code gen

--- JS/codegen.js
+++ JS/codegen.js
@@ -1,5 +1,18 @@
-
+var tree;
+var code = [];
+var cErrors = 0;
+var cWarnings = 0;
 
 function gen(ast) {
+    tree = ast;
+    code = [];
+    cErrors = 0;
+    cWarnings = 0;
 
+    code = generate();
+    
+    return code;
+}
+
+function generate() {
 }
