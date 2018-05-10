9240375eb1b7f3a3578e157310be71eae8520f51
Chris Siena <zenatron@gmail.com>
2018-05-10 00:22:59 -0400

    Corrected two catch statements

--- JS/compiler.js
+++ JS/compiler.js
@@ -97,7 +97,7 @@ jQuery.fn.scrollTo = function(elem, speed) {
 		$(this).animate({
 			scrollTop: $(this).scrollTop() - $(this).offset().top + $(elem).offset().top
 		}, speed == undefined ? 1000 : speed); 
-	} catch {}
+	} catch (e) {}
     return this; 
 };
 
